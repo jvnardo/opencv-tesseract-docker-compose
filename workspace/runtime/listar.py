242b3725a67f19efe6ad56b6edd64ff36bab7b5b
jvnardo <113140453+jvnardo@users.noreply.github.com>
2022-09-22 14:34:07 -0300

    Delete listar.py

--- workspace/runtime/listar.py
+++
@@ -1,9 +0,0 @@
-#for imageName in os.listdir(path): 
-            #inputPath = os.path.join(path, imageName) 
-        #img = Image.open(inputPath) 
-        #text = pt.image_to_string(img, lang ="eng") 
-        #imagePath = imagePath[0:-4]
-
-import os
-for _, _, j in os.walk('/workspace'):
-    print(j)
