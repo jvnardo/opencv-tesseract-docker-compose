f880db39a135c86e940e1683ac0179816909ac81
jvnardo <113140453+jvnardo@users.noreply.github.com>
2022-09-22 14:35:43 -0300

    atts

--- workspace/runtime/converter.py
+++ workspace/runtime/converter.py
@@ -5,10 +5,8 @@ import os
 import glob,os.path
 
 
-imagems = (glob.glob('imagens/*'))
-#imagems = (os.listdir ())
+imagems = (glob.glob('/usr/image/*'))
 for imagem in imagems:
 	img = cv2.imread(imagem)
 	resultado = pytesseract.image_to_string(img)
 	print(resultado)
-#print (imagems)
